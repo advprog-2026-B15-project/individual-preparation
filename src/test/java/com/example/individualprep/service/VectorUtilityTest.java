5aa5359d6aac17b3ff5831d971f3d37f2d9e642a
AryNov <arya.nov.pratama@gmail.com>
2026-02-13 14:51:51 +0700

    implemented a unit test for the dotproduct feature of vector utility

--- src/test/java/com/example/individualprep/service/VectorUtilityTest.java
+++ src/test/java/com/example/individualprep/service/VectorUtilityTest.java
@@ -1,6 +1,8 @@
 package com.example.individualprep.service;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
@@ -13,5 +15,41 @@ class VectorUtilityTest {
         double[] result = vectorUtility.multiply(new double[] { 1.5, -2.0, 0.25 }, -3);
 
         assertArrayEquals(new double[] { -4.5, 6.0, -0.75 }, result, 1e-9);
+    }
+
+    @Test
+    void dotProductReturnsValid(){
+        VectorUtility vectorUtility = new VectorUtility();
+
+        double[] v1_test = {1,2.5,3};
+        double[] v2_test = {1,2.5,3};
+
+        double result = vectorUtility.dotProduct(v1_test,v2_test);
+        assertEquals(16.25, result,1e-9);
+    }
+
+    @Test
+    void dotProductThrowsNull() {
+        VectorUtility vectorUtility = new VectorUtility();
+
+        assertThrows(IllegalArgumentException.class,
+                () -> vectorUtility.dotProduct(null, new double[]{1, 2})
+        );
+
+        assertThrows(IllegalArgumentException.class,
+                () -> vectorUtility.dotProduct(new double[]{1, 2}, null)
+        );
+    }
+
+    @Test
+    void dotProductThrowsDifferentLengths() {
+        VectorUtility vectorUtility = new VectorUtility();
+
+        assertThrows(IllegalArgumentException.class,
+                () -> vectorUtility.dotProduct(
+                        new double[]{1, 2},
+                        new double[]{1, 2, 3}
+                )
+        );
     }
 }
